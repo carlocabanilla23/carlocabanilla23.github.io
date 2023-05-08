0dbd74a680fcb87bdeda723bc9d57e2e78809805
John Carlo Cabanilla <cabanillaj@spu.edu>
2023-05-08 12:25:12 -0700

    gh page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
+
 })
 export class AppComponent {
   title = 'portfolio';
